6ecf8663cff8df0d0751a47d5312ef5b1aed9f34
Farmix2003 <ffarrux386@gmail.com>
2023-08-24 15:02:44 +0300

    Some Correction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/nav/Navbar"
 import { BrowserRouter } from "react-router-dom"
 import Rout from "./components/route"
-import Footer from "./components/footer/Footer"
+import Footer from "./components/Footer/Footer"
 import { useState } from "react"
 import Productdetails from "./components/ProductDetails"
 const App = () => {
